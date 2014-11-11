68baf14733ced88ebce98f8c3c1ebeb2a6276ec1
smidori <smidori@gmail.com>
2014-11-11 21:41:12 -0200

    third commit - primeiro helloworld realizado

---
+++ HelloAndroid/src/br/com/mobile/helloandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.mobile.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
